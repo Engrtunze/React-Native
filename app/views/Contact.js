3401385d5d135c0e6056a581fbc6008c0c07d151
tunze <tunde4excel@gmail.com>
2020-05-15 16:09:40 +0100

    added form to contact

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput, TouchableHighlight, Alert} from 'react-native';
 import {Header} from "../sections/Header";
-import {Hero} from "../sections/Hero";
-import {Menu} from "../sections/Menu";
+//import  {createStackNavigator} from '@react-navigation/native';
+
 
 
 
@@ -10,6 +10,23 @@ export class Contact extends React.Component{
     static navigationOptions = {
 
         headerShown : false,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            msg : 'Enter Message',
+            name : 'Enter Name',
+            email : 'Enter your Email Address'
+        }
+    }
+
+    clearFields=()=> this.setState({name:'',msg:'', email:''});
+
+    sendMessage=()=>
+    {
+        Alert.alert(this.state.name,this.state.msg);
+         this.props.navigation.goBack();
     }
 
 
@@ -17,8 +34,40 @@ export class Contact extends React.Component{
         return (
             <View style={styles.container}>
                 <Header message='Press to Login'/>
-                <Text style={{flex: 8}}>The Contact form will go here</Text>
-                <Text style={{flex: 6}}>The Contact form will go here</Text>
+                <Text style={styles.heading}>Contact Us</Text>
+
+                <TextInput
+                    style={styles.inputs}
+                    onChangeText={(text) => this.setState({name: text})}
+                    value = {this.state.name} />
+
+                <TextInput
+                    style={styles.inputs}
+                    onChangeText={(text) => this.setState({msg: text})}
+                    value = {this.state.msg}
+                    multiline = {true}
+                    numbberOfLines ={4}
+                />
+                <TextInput
+                    style={styles.inputs}
+                    onChangeText={(text) => this.setState({email: text})}
+                    value = {this.state.email}
+
+                />
+
+                <TouchableHighlight onPress={this.sendMessage} underlayColor='#31e981'>
+                    <Text style = {styles.buttons}>
+                        send Message
+                    </Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight onPress={()=>this.clearFields()} underlayColor='#31e981'>
+                    <Text style = {styles.buttons}>
+                        Reset Form
+                    </Text>
+                </TouchableHighlight>
+
+
             </View>
 
         );
@@ -29,7 +78,28 @@ export class Contact extends React.Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        paddingBottom: '45%'
+    },
+    heading: {
+        fontSize: 16,
+        flex: 1
+    },
+    inputs:
+        {
+            flex: 1,
+            width: '80%',
+            padding: 10
+        },
 
+    multiInput: {
+        flex: 2,
+        width: '90%',
+        paddingTop: 20
+    },
+    buttons: {
+        marginTop: 15,
+        fontSize: 16
     }
 
 });
